9d99e0a0deea72cf37dca1a5b0d877a8a30a1f72
alice15311 <alice15311@gmail.com>
2020-12-17 20:57:37 -0500

    created code with falcon drive subsystem and encoders
    pls check if encode work and is there thx

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants 
+{
+    //Drivetrain constants
+    public static final int RIGHT_TALON_ONE = 0;
+    public static final int RIGHT_TALON_TWO = 1;
+    public static final int LEFT_TALON_ONE = 2;
+    public static final int LEFT_TALON_TWO = 3;
+
+    public static final int LEFT_ENCODER_A = 0;
+    public static final int LEFT_ENCODER_B = 1;
+    public static final int RIGHT_ENCODER_A = 2;
+    public static final int RIGHT_ENCODER_B = 3;
+
+    public static final int DISTANCE_PER_PULSE = 2048;
+}
---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,73 @@
+package frc.robot.subsystems;
+
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+
+    private TalonFX rightTalon1;
+    private TalonFX rightTalon2;
+    private TalonFX leftTalon1;
+    private TalonFX leftTalon2;
+
+    private Encoder leftEncoder, rightEncoder;
+
+    private DrivetrainSubsystem() {
+        rightTalon1 = new TalonFX(Constants.RIGHT_TALON_ONE);
+        rightTalon2 = new TalonFX(Constants.RIGHT_TALON_TWO);
+        leftTalon1 = new TalonFX(Constants.LEFT_TALON_ONE);
+        leftTalon2 = new TalonFX(Constants.LEFT_TALON_TWO);
+
+        rightTalon1.setNeutralMode(NeutralMode.Brake);
+        rightTalon2.setNeutralMode(NeutralMode.Brake);
+        leftTalon1.setNeutralMode(NeutralMode.Brake);
+        leftTalon2.setNeutralMode(NeutralMode.Brake);
+
+        leftEncoder = new Encoder(Constants.LEFT_ENCODER_A, Constants.LEFT_ENCODER_B);
+        rightEncoder = new Encoder(Constants.RIGHT_ENCODER_A, Constants.RIGHT_ENCODER_B);
+
+    }
+
+    public void drive(double leftpower, double rightpower){
+        rightTalon1.set(ControlMode.PercentOutput, rightpower);
+        rightTalon2.set(ControlMode.PercentOutput, rightpower);
+        leftTalon1.set(ControlMode.PercentOutput, leftpower);
+        leftTalon2.set(ControlMode.PercentOutput, leftpower);
+    }
+
+    public void stopDrive(){
+        rightTalon1.set(ControlMode.PercentOutput, 0);
+        rightTalon2.set(ControlMode.PercentOutput, 0);
+        leftTalon1.set(ControlMode.PercentOutput, 0);
+        leftTalon2.set(ControlMode.PercentOutput, 0);
+    }
+
+    public double getLeftEncoder() {
+        return (leftEncoder.get() * Constants.DISTANCE_PER_PULSE);
+    }
+
+    public double getRightEncoder() {
+        return (-rightEncoder.get() * Constants.DISTANCE_PER_PULSE);
+    }
+
+    public double getAverageEncoder() {
+        return (getLeftEncoder() + getRightEncoder()) / 2;
+    }
+
+    public void resetEncoder() {
+        leftEncoder.reset();
+        rightEncoder.reset();
+    }
+
+    public void driveWithMinPower(double leftPower, double rightPower, double minAbsolutePower) {
+        double realLeftPower = (leftPower / Math.abs(leftPower)) * Math.max(Math.abs(leftPower), minAbsolutePower);
+        double realRightPower = (rightPower / Math.abs(rightPower)) * Math.max(Math.abs(rightPower), minAbsolutePower);
+    }
+}
+
