2d2b6ab69ed993676e06a12b1b9dcf7673a6c660
Dutivax <putnamdustin@gmail.com>
2021-02-20 18:50:12 -0500

    slalom testing

--- src/main/java/frc/robot/commands/drivetrain/SlalomTestCommandGroup.java
+++ src/main/java/frc/robot/commands/drivetrain/SlalomTestCommandGroup.java
@@ -9,16 +9,19 @@ public class SlalomTestCommandGroup extends SequentialCommandGroup {
         super();
         addCommands(
             // brute force of the slalom course
-                new DriveForDistanceCommand(drivetrainSubsystem, 72/2, 0.5, 0.5),
-                new TurnToAnglePIDCommand(35, drivetrainSubsystem),
+                new DriveForDistanceCommand(drivetrainSubsystem, 8, 0.5, 0.5),
+                new TurnToAnglePIDCommand(45, drivetrainSubsystem),
                 new DriveForDistanceCommand(drivetrainSubsystem, 84, 0.5, 0.5),
-                // new TurnToAnglePIDCommand(-, drivetrainSubsystem),
-                // new DriveForDistanceCommand(drivetrainSubsystem, 46/2, 0.5, 0.5),
-                new TurnToAnglePIDCommand(-90, drivetrainSubsystem),
-                new DriveForDistanceCommand(drivetrainSubsystem, 50/2, 0.5, 0.5),
+                new TurnToAnglePIDCommand(-45, drivetrainSubsystem),
+                new DriveForDistanceCommand(drivetrainSubsystem, 64, 0.5, 0.5),
+                new TurnToAnglePIDCommand(-45, drivetrainSubsystem),
+                new DriveForDistanceCommand(drivetrainSubsystem, 48, 0.5, 0.5),
                 new TurnToAnglePIDCommand(90, drivetrainSubsystem),
-                new DriveForDistanceCommand(drivetrainSubsystem, 50/2, 0.5, 0.5),
-                new TurnToAnglePIDCommand(90, drivetrainSubsystem)
+                new DriveForDistanceCommand(drivetrainSubsystem, 24, 0.5, 0.5)
+
+                // new TurnToAnglePIDCommand(90, drivetrainSubsystem),
+                // new DriveForDistanceCommand(drivetrainSubsystem, 84, 0.5, 0.5),
+                // new TurnToAnglePIDCommand(90, drivetrainSubsystem)
         );
     }
 }
